08d27ef233fac6175729115ae490a40064d83f63
Alejandro <42217598+dazm1710@users.noreply.github.com>
2019-07-21 11:46:24 -0500

    Agregacion Juegos Compañeros y Adicionales
    Se agregan cambios, una tabla donde estan los juegos y se le pone etiqueta a las imagenes de cada juego

---
+++ Juegos/Gallina4/js/gallina.js
@@ -0,0 +1,73 @@
+function Gallina(){
+this.x = 310; this.y = 15;
+ this.img = [$("#abajo")[0],$("#arriba")[0],$("#salto")[0],$("#sentado")[0]]; this.sprite = 0;
+ this.vida = 1;
+ this.puntos = 0;
+ this.seguro = "arriba";
+ this.dibujar = function(ctx){
+ var img = this.img[this.sprite];
+ var x = this.x;
+ var y = this.y;
+ ctx.drawImage(img, x, y); ctx.save();
+ ctx.fillStyle = "#ffffff";
+ ctx.font = "12px sans-serif";
+ ctx.fillText("Puntaje: "+ this.puntos, 10,70);
+ if(this.sprite==2){
+ ctx.fillStyle = "#ff0000";
+ ctx.font = "20px sans-serif";
+
+}
+	ctx.restore();
+	}
+
+
+this.actualizar = function(accion){
+if(accion=="arriba" && this.y > 15){
+	this.y -= 10;
+//this.sprite = 1;
+	}
+
+
+if(accion=="abajo"  && this.y < 390){
+this.y += 10;
+//this.sprite = 0;
+}
+
+if(accion=="izquierda"){
+this.x -= 10;
+this.sprite = 1;
+}
+
+
+if(accion=="derecha"){
+this.x += 10;
+this.sprite = 0;
+}
+
+
+if(this.y > 340 && this.seguro == "arriba"){
+			this.seguro = "abajo";
+			this.puntos++;
+		}
+	
+
+if(this.y < 20 && this.seguro == "abajo"){
+			this.seguro = "arriba";
+			this.puntos++;
+		}
+	}
+	
+
+
+
+
+this.colision = function(x,y){
+var distancia=Math.sqrt( Math.pow( (x-this.x), 2)+Math.pow( (y-this.y),2));
+if(distancia>this.img[this.sprite].width)
+return false;
+		else
+ return true;}
+}
+
+									
+	
---
+++ Juegos/Gallina4/js/script.js
@@ -0,0 +1,93 @@
+
+var jugando;
+
+$(document).ready(inicio);
+$(document).keydown(capturaTeclado);
+
+function inicio(){
+	jugando = true;
+	miCanvas = $("#mi_canvas")[0];
+	contexto = miCanvas.getContext("2d");
+	buffer = document.createElement("canvas");
+	gallina = new Gallina();
+	carro = [new Carro(), new Carro(),
+				   new Carro(), new Carro(), new Carro()];
+	run();	
+	
+	$('#instrucciones').click(function(){
+        $('#popup').fadeIn('slow');
+        $('.popup-overlay').fadeIn('slow');
+        $('.popup-overlay').height($(window).height());
+        return false;
+    });
+    
+    $('#close').click(function(){
+        $('#popup').fadeOut('slow');
+        $('.popup-overlay').fadeOut('slow');
+        return false;
+    });
+    
+    $("#iniciar").click(function(){	
+		if(jugando==false)
+			inicio();	
+	});
+}
+
+function capturaTeclado(event){
+	if(event.which==38 || event.which==87)
+		gallina.actualizar('arriba');
+	if(event.which==40 || event.which==83)
+		gallina.actualizar('abajo');
+	if(event.which==39 || event.which==68)
+	gallina.actualizar('derecha');
+	if(event.which==37 || event.which==65)
+		gallina.actualizar('izquierda');
+	
+}
+
+function run(){ 
+	buffer.width = miCanvas.width;
+	buffer.height = miCanvas.height;
+	contextoBuffer = buffer.getContext("2d");
+		 
+	if(jugando){  
+		contextoBuffer.clearRect(0,0,buffer.width,buffer.height);
+
+		gallina.dibujar(contextoBuffer);
+		for(i=0;i<carro.length;i++){
+			carro[i].dibujar(contextoBuffer);
+			carro[i].actualizar();
+			if(gallina.colision(carro[i].x+40,carro[i].y)){
+				gallina.sprite = 2;
+				gallina.vida--;
+				$('#pierde')[0].play();
+			}
+		}
+		
+		if(gallina.vida <= 0)
+			jugando = false;
+		
+		contexto.clearRect(0,0,miCanvas.width,miCanvas.height);
+		contexto.drawImage(buffer, 0, 0);
+		setTimeout("run()",20);
+		
+	}else{
+		contextoBuffer.clearRect(0,0,buffer.width,buffer.height);
+		contextoBuffer.fillStyle = "#ffffff";
+		gallina.sprite = 3;
+		gallina.vida = 0;
+		gallina.dibujar(contextoBuffer);
+	
+
+
+contextoBuffer.fillStyle = "red";	contextoBuffer.font = "50px sans-serif";
+		
+contextoBuffer.fillText("PERDISTE", 200, 50);
+		contextoBuffer.fillStyle = "red";
+		contexto.clearRect(0,0,miCanvas.width,miCanvas.height);
+		contexto.drawImage(buffer, 0, 0);
+	}
+	
+}
+
+
